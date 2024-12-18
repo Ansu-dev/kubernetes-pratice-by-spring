d0a382da8a6dc6b6d65bbad66bbc6cff02b04fa2
Ansu-dev <ansu1007@naver.com>
2024-12-18 15:23:34 +0900

    🔐 Add: deployment env 추가

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
+    // 환경변수를 받아와 코드에 출력
+    @Value("${MY_ACCOUNT:default}")
+    private String myAccount;
+    @Value("${MY_PASSWORD:default}")
+    private String myPassword;
+
     @GetMapping("/")
     public String home() {
         return "Version 1.0";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "MyAccount: " + myAccount + ", MyPassword: " + myPassword;
     }
 }
